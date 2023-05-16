8c301ecb3313721671d130e67e27b0e79a1b45dd
Asius22 <ciro_2000@live.it>
2023-05-16 13:21:45 +0200

    created ica files

--- events_reader.py
+++ events_reader.py
@@ -1,25 +1,72 @@
 import scipy.io as scipy
+import numpy as np
 
-image_duration=5
-rest_duration=120
-
-
-events = scipy.loadmat("RAW_PARSED/s1_s1.mat")['events']
-event_dict = []
 event_array = []
 """
     # ciclo per creare l'event:dict
     # deve contenere la label dell'evento e l'id dell'evento
     # il tipo dell'evento si trova nel percorso 2 0 0 0 0
 """
-#types = ['IMAGE', 'COGNITIVE', 'SSVEPC', 'SSVEP', 'REST', 'EYES']
-types = dict()
+event_mapping = {'IMAGE': 1, 'COGNITIVE': 5, 'SSVEPC': 201, 'SSVEP': 211, 'REST': 3, 'EYES': 111}
 
-for i in range(0, len(events)):
-    event = events[i]
-    type = event[2][0][0][0][0]
+# funzione di normalizzazione dei file *.mat
 
-    print(event[2])
-    key = f"{type} {i}"
-    event_dict.append({key: i})
 
+def normalizaParsed():
+    n_pers = 21
+    n_esp = 3
+    for i in range(1, n_pers + 1):  # apri tutti i file
+        for j in range(1, n_esp + 1):
+            save_mat = False
+            mat = scipy.loadmat(f"RAW_PARSED/s{i}_s{j}.mat")
+            print(f"-------------------s{i}_s{j}--------------------------------")
+            events = mat['events']  # carica gli eventi
+            for event in events:  # gli eventi sono o in posizione 2 0 0 0 0 o in posizione 2 0 0 3 0
+                final_event_type = event[2][0][0][0][0]
+                if final_event_type.dtype.type is not np.str_ or not event_mapping.__contains__(
+                        final_event_type):  # controlla che in tutti i file l'occorrenza 2 0 0 0 0 sia una stringa
+                    save_mat = True
+                    print(event[2][0][0])
+                    try:
+                        if type(event[2][0][0][1][0]) is np.str_ and event_mapping.__contains__(event[2][0][0][1][0]):
+                            event[2][0][0][0], event[2][0][0][1] = event[2][0][0][1], event[2][0][0][0]
+                            # print("è 1")
+                        elif type(event[2][0][0][2][0]) is np.str_ and event_mapping.__contains__(event[2][0][0][2][0]):
+                            event[2][0][0][0], event[2][0][0][2] = event[2][0][0][2], event[2][0][0][0]
+                            # print("è 2")
+                        else:
+                            event[2][0][0][0], event[2][0][0][3] = event[2][0][0][3], event[2][0][0][0]
+                            # print("è 3")
+                    except IndexError:
+                        print(f"----------------{event}")
+
+                    #print("-----" + event[2][0][0][0][0])
+
+            if save_mat:
+                scipy.savemat(f"RAW_PARSED/s{i}_s{j}.mat", mdict=mat)
+
+
+def read_event(path):
+    events = scipy.loadmat(path)['events']
+    event_start = events[0][0][0][0]
+    for event in events:
+        # event = events[i]
+        # 2 0 0 0 0
+        event_type = event[2][0][0][0][0]
+        print(event_type)
+
+        # la prima colonna contiene l'inizio dell'evento
+        first_column = event[0][0][0] - event_start
+        # la seconda colonna contiene la fine dell'evento
+        second_column = event[1][0][0] - event_start
+        # la terza colonna contiene il tipo di evento che si è presentato
+        third_column = event_mapping[event_type]
+        array = np.array([first_column, second_column, third_column], dtype=np.int64)
+        event_array.append(array)
+
+    # TODO: sort event_array
+    event_array.sort(key=lambda x: x[0])
+    return event_mapping, np.asarray(event_array)
+
+
+# read_event("RAW_PARSED/s3_s2.mat")
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import scipy.io as scipy
-import os
 import mne
 from autoreject import get_rejection_threshold
+from events_reader import read_event
 
-import matplotlib.pyplot as pl
-from sklearn.datasets import load_digits
-from sklearn.decomposition import FastICA
-#                     *    CODICE FUNZIONANTE PER CREAZIONE GRSFIIC(import shcipy.io, pandas e mne)    *
 # should be 21
-nPart = 1
+nPart = 21
 # should be 3
-nEsp = 1
+nEsp = 3
 # filtering const
 low_cut_freq = 0.5
 high_cut_freq = 45
@@ -18,7 +14,6 @@ tstep = 1.0
 # ICA properties
 random_state = 42
 ica_n_components = .99 # ica must mantain 99% of data
-ica = mne.preprocessing.ICA(n_components=ica_n_components, random_state=random_state)
 # .mat file directory name
 DIRECTORY_NAME = "RAW_PARSED"
 cols = ["COUNTER", "INTERPOLATED", "F3", "FC5", "AF3", "F7", "T7", "P7", "O1", "O2", "P8", "T8", "F8", "AF4", "FC6",
@@ -29,27 +24,35 @@ ten_twenty_montage = mne.channels.make_standard_montage("standard_1020")
 
 for i in range(1, nPart + 1):
     for j in range(1, nEsp + 1):
+        ica = mne.preprocessing.ICA(n_components=ica_n_components, random_state=random_state)
+        path = f'{DIRECTORY_NAME}/s{i}_s{j}.mat'
         # load the .mat file
-        mat = scipy.loadmat(f'{DIRECTORY_NAME}/s{i}_s{j}.mat')
+        mat = scipy.loadmat(f"{DIRECTORY_NAME}/s2_s1.mat")
         # fetch recording data
         raw_input = mat['recording']
-        print(mat['events'][1:1])
+        # carica eventi
+        events = mat['events']
+        event_mapping, event_array = read_event(f"{DIRECTORY_NAME}/s2_s1.mat")
+
         # create the raw array with correct metadata
         raw = mne.io.RawArray(list(map(lambda x: x /1000000, raw_input.transpose())), info)
         raw.info["bads"] += ["COUNTER", "INTERPOLATED", "...UNUSED DATA..."]
         raw.set_montage(ten_twenty_montage,  on_missing="ignore")
         eeg_channels = mne.pick_types(raw.info, eeg=True)
-
+        # plot pre filtering
         # raw.plot(title="Raw Array", start=15, duration=5, order=eeg_channels, n_channels=len(eeg_channels), scalings='auto')
         # start digital filtering and plot result
-        raw_filtered = raw.filter(low_cut_freq, high_cut_freq, "all")
-        raw_filtered.compute_psd(fmax=100).plot()
-        raw_filtered.plot(title="filtered", duration=60, order=eeg_channels, n_channels=len(eeg_channels), scalings='auto')
-
-        raw_events = mne.io.RawArray(mat['events'])\
-            .filter(low_cut_freq, high_cut_freq, 'all')
-        events_ica = mne.make_fixed_length_events(raw_events, duration=tstep)
-        epochs_ica = mne.Epochs(raw_filtered, events_ica, tmin=0.0, tmax=tstep, baseline=None, preload=True)
+        raw_filtered = raw.copy().filter(low_cut_freq, high_cut_freq, "all")
+        # raw_filtered.compute_psd(fmax=100).plot()
+        # plot post filtering
+        # raw_filtered.plot(title="filtered", duration=60, order=eeg_channels, n_channels=len(eeg_channels), scalings='auto')
+        # create epochs with recording and events properly preprocessed
+        epochs_ica = mne.Epochs(raw_filtered, event_array, event_id=event_mapping, tmin=0, tmax=tstep, baseline=None, preload=True, event_repeated='merge')
         reject = get_rejection_threshold(epochs_ica)
         ica.fit(epochs_ica, reject=reject, tstep=tstep)
-        ica.plot_components()
+        epochs_post_ica = ica.apply(epochs_ica.copy())
+        # epochs_post_ica.plot()
+        ica.save(f"./ICA/s{i}_s{j}-ica.fif", overwrite=True)
+        print(f"j----------------------------------{j}")
+    print(f"i----------------------------------{i}")
+exit()
